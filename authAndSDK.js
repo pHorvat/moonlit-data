9cf1c175d2d783699622f833b6ae2ce523392978
pHorvat <hpatrik.44@gmail.com>
2020-08-12 12:50:00 +0200

    v2.10

--- authAndSDK.js
+++ authAndSDK.js
@@ -51,6 +51,8 @@ window.onSpotifyPlayerAPIReady = () => {
         console.log(state)
         $('#current-track').attr('src', state.track_window.current_track.album.images[0].url);
         $('#current-track-name').text(state.track_window.current_track.name);
+        $('#current-track-artist').text(state.track_window.current_track.artist.name);
+
     });
 
     // Ready
@@ -58,7 +60,7 @@ window.onSpotifyPlayerAPIReady = () => {
         console.log('Ready with Device ID', data.device_id);
 
         // Play a track using our new device ID
-        play(data.device_id);
+        //play(data.device_id);
     });
 
     // Connect to the player!
