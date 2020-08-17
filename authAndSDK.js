56aba78c5f8588d62230aac37c915037d98ecafc
pHorvat <hpatrik.44@gmail.com>
2020-08-17 17:33:49 +0200

    Update authAndSDK.js

--- authAndSDK.js
+++ authAndSDK.js
@@ -36,7 +36,7 @@ if (!_token) {
 
 window.onSpotifyPlayerAPIReady = () => {
     const player = new Spotify.Player({
-        name: 'Web Playback SDK Template',
+        name: 'Moonlit Web Player',
         getOAuthToken: cb => { cb(_token); }
     });
 
