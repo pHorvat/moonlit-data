4ba308a2bc45f0e767ee4b53b07c7ada40ea3c10
pHorvat <hpatrik.44@gmail.com>
2020-08-21 13:36:05 +0200

    Update authAndSDK.js

--- authAndSDK.js
+++ authAndSDK.js
@@ -145,7 +145,7 @@ function play(device_id) {
     $.ajax({
         url: "https://api.spotify.com/v1/me/player/play?device_id=" + device_id,
         type: "PUT",
-        data: '{"uris": ["spotify:track:6jQPolTBq89z9AHrMrmYpS"]}',
+        data: '{"uris": ["spotify:track:6GnoxbkPzqSFqvfXtosJ1W"]}',
         beforeSend: function(xhr){xhr.setRequestHeader('Authorization', 'Bearer ' + _token );},
         success: function(data) {
             console.log(data)
