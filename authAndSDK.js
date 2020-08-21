d38a5852d9f62796c9c55333654394f0c7fa1275
pHorvat <hpatrik.44@gmail.com>
2020-08-21 12:51:25 +0200

    Update authAndSDK.js

--- authAndSDK.js
+++ authAndSDK.js
@@ -51,7 +51,7 @@ window.onSpotifyPlayerAPIReady = () => {
         console.log(state)
         $('#current-track').attr('src', state.track_window.current_track.album.images[2].url);
         $('#current-track-name').text(state.track_window.current_track.name);
-        $('#current-track-artist').text(state.track_window.current_track.artists[0].name);
+        $('#current-track-artist').text(state.track_window.current_track.artists[0].name+", "+state.track_window.current_track.artists[1].name+","+state.track_window.current_track.artists[2].name);
         document.getElementById("optionalCurrent").style.visibility = "visible";
         document.getElementById("optionalCurrent2").style.visibility = "visible";
         if(state.paused===true){
