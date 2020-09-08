d031ecab50bc114383c1096b52cc062f4ba00692
pHorvat <hpatrik.44@gmail.com>
2020-09-08 19:01:24 +0200

    Update authAndSDK.js

--- authAndSDK.js
+++ authAndSDK.js
@@ -193,7 +193,7 @@ function play(device_id) {
             'device_id': device_id
         },
         data:{
-            'uris': SpotifyURIs.join('","')
+            'uris': 'spotify:track:57724RmJKfjECtItkTmVZI'
         },
         headers: {
             'Authorization': 'Bearer '+ _token,
