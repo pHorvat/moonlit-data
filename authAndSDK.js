edbce701b78f801ecde5e93a6c201d8d469d8d25
pHorvat <hpatrik.44@gmail.com>
2020-09-11 21:48:34 +0200

    Update authAndSDK.js

--- authAndSDK.js
+++ authAndSDK.js
@@ -277,7 +277,7 @@ function requestArists(tempIDs){
         let r = 0;
         while(r!==5){
             var el = document.getElementById('artistLink'+r);
-            if (el!=null){
+            if (el!==null){
                 el.remove();
             } // Removes the div with the 'div-02' id
             r++;
