c32721c7e8bd32c31122c46467756099189d235c
pHorvat <hpatrik.44@gmail.com>
2020-09-11 16:56:18 +0200

    Update authAndSDK.js

--- authAndSDK.js
+++ authAndSDK.js
@@ -82,7 +82,7 @@ window.onSpotifyPlayerAPIReady = () => {
         }
         //var artistsJoined = artistsArr.join(', ');
         //$('#current-track-artist').text(artistsJoined);
-        requestArists(state.track_window.current_track.artists);
+        requestArists(state.track_window.current_track.artists.uri);
 
 
         document.getElementById("optionalCurrent").style.visibility = "visible";
