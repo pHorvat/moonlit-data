f5b0be4ee2af6ff36155581aee3c21a327958053
pHorvat <hpatrik.44@gmail.com>
2020-09-11 22:19:49 +0200

    Update authAndSDK.js

--- authAndSDK.js
+++ authAndSDK.js
@@ -297,7 +297,7 @@ function requestArists(tempIDs){
             var link = document.createTextNode(artistData.data.artists[i].name+"  ");
             a.appendChild(link);
             //a.title = artistsArr[i];
-            a.href = data.artists[i].images[0].url;
+            a.href = response.artists[i].images[0].url;
             a.id = elementID;
             const artistLink = document.getElementById("current-track-artist");
             var parentDiv = document.getElementById("parentElement");
